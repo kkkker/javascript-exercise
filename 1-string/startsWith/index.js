3ddbd531d417535cb8281b017da9b96160ef3444
kker <907765631@qq.com>
2020-09-21 15:19:11 +0800

    [Mamingjun]: implement startsWith

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,9 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  return collection.reduce((acc, cur) => {
+    if (cur.startsWith('粤A')) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
 }
